a9f83e9f5a4fc3032a6c605b6139296b3fa17b2b
Riccardo Galeazzi <galeazziriccardo01@gmail.com>
2024-09-02 19:30:58 +0200

    FirstCommit

---
+++ script.js
@@ -0,0 +1,53 @@
+function hamburg(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
+}
+
+function cancel(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}
+
+// Typewriter Effect
+
+const texts = [
+    "Game Developer",
+    
+]
+
+let speed  =100;
+const textElements = document.querySelector(".typewriter-text");
+
+let textIndex = 0;
+let charcterIndex = 0;
+
+function typeWriter(){
+    if (charcterIndex < texts[textIndex].length){
+        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
+        charcterIndex++;
+        setTimeout(typeWriter, speed); 
+    }
+    else{
+        setTimeout(eraseText, 1000)
+    }
+}
+
+function eraseText(){
+    if(textElements.innerHTML.length > 0){
+        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
+        setTimeout(eraseText, 50)
+    }
+    else{
+        textIndex = (textIndex + 1) % texts.length;
+        charcterIndex = 0;
+        setTimeout(typeWriter, 500)
+    }
+}
+
+window.onload = typeWriter
+
+// Get the element with the scrollbar
+const certificatesGrid = document.querySelector('.Certificates-grid');
+
+// Add a class to the element to hide the scrollbar
+certificatesGrid.classList.add('hide-scrollbar');
